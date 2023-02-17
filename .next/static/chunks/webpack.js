c34e494be26a85a10008c67aba3c657412cfc3de
HrantDeveloper <hrantsargsyan6@gmail.com>
2023-02-17 07:37:09 +0000

    master

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b208c35d041ca8a"; }
+/******/ 		__webpack_require__.h = function() { return "5530b33d775c8256"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
